c1c667c20da7a5d21bf5d57d91600ee17e19e96d
ANonABento <kevin.jiang53@gmail.com>
2025-09-19 16:04:22 -0400

    add header for node costmap

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -14,10 +14,18 @@ class CostmapNode : public rclcpp::Node {
     void publishMessage();
  
   private:
-    robot::CostmapCore costmap_;
-    // Place these constructs here
+    // Callbacks
+    void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan);
+    void publishCostmap();
+    void publishMessage();
+
+    // Publishers / Subscribers
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_pub_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
-};
+
+    // Core costmap logic
+    robot::CostmapCore costmap_core_;
  
 #endif 
